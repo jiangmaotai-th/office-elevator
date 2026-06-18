a945c84e85702a65642a3e61d27c8a86fafe2ad4
江天宏 <jiangtianhong@jiangtianhongdeMacBook-Pro.local>
2026-06-18 18:21:09 +0800

    Build cache-busted GitHub Pages bundle

--- docs/index.39fe6.js
+++ docs/index.39fe6.js
@@ -1,4 +1,4 @@
-System.register(["./application.js"], function (_export, _context) {
+System.register(["./application.b9907.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
